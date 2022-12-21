8bcf5dd159f27e41a73ca2cfd099d4fa8c722943
FuexFollets <fuex9follets@gmail.com>
2022-12-21 19:51:57 +0530

    Added option listing

--- src/context_parser.hpp
+++ src/context_parser.hpp
@@ -1,13 +1,27 @@
 #pragma once
 
+#include <cstddef>
 #include <vector>
 #include <string_view>
 #include <string>
 #include <algorithm>
 #include <random>
+#include <span>
 
 class context_parser {
     public:
+
+    static constexpr const std::size_t available_option_count {};
+
+    struct option {
+        const std::string_view name;
+        const std::string_view description;
+
+        bool is_same_as_another {};
+        const option* const same_option;
+    };
+
+    static constexpr const std::array<option, available_option_count> all_options {};
 
     context_parser() = default;
     context_parser(int argc, const char** argv);
@@ -16,7 +30,7 @@ class context_parser {
     std::vector<std::string> args_list;
 
     std::string test_name {}; // Name of directory test is stored in
-    std::string test_directory {}; // Name of directory to create directory which the test is store in
+    std::string test_path {}; // Path to the directory in which the test directory will be stored (defaults to $home/tests)
     std::string current_directory {}; // Current directory at runtime
 
     bool is_in_test_directory {};
