da36b5bb969c8105bd1dc548864393a622a1227a
FuexFollets <fuex9follets@gmail.com>
2022-12-21 21:35:10 +0530

    Implemented option initializers

--- src/context_parser.hpp
+++ src/context_parser.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstddef>
+#include <type_traits>
 #include <vector>
 #include <string_view>
 #include <string>
@@ -11,9 +12,7 @@
 
 namespace constraints {
     template <typename T, typename type_constructed>
-    concept can_construct = requires(T value) {
-        type_constructed(value);
-    };
+    concept can_construct = std::is_constructible<type_constructed, T>::value;
 }
 
 class context_parser {
@@ -22,6 +21,7 @@ class context_parser {
     static constexpr const std::size_t available_option_count {};
 
     struct option {
+        option() = default;
         option(const option&) = default;
 
         option(constraints::can_construct<std::string> auto, constraints::can_construct<std::string> auto);
@@ -31,7 +31,7 @@ class context_parser {
         const std::string_view description;
 
         bool is_same_as_another {};
-        const option* const same_option;
+        const option* same_option {nullptr};
     };
 
     static constexpr const std::array<option, available_option_count> all_options {};
@@ -49,3 +49,16 @@ class context_parser {
     bool is_in_test_directory {};
     bool remove_when_done {};
 };
+
+context_parser::option::option(
+        constraints::can_construct<std::string> auto option_name,
+        constraints::can_construct<std::string> auto option_description) : 
+    name {std::move(option_name)}, description {std::move(option_description)} {}
+
+context_parser::option::option(
+        constraints::can_construct<std::string> auto option_name,
+        option* option_same_as_this) : name {std::move(option_name)}, is_same_as_another {true}, same_option {option_same_as_this} {
+    option_same_as_this -> is_same_as_another = true;
+    option_same_as_this -> same_option = this;
+}
+
