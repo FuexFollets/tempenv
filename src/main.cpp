cb6d4b776941a9fa4a247e3a6aa644ca227c4b30
FuexFollets <fuex9follets@gmail.com>
2022-12-23 21:27:14 +0530

    Initalized context_parser in main method

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+
+#include "context_parser.hpp"
+
+int main(int argc, const char** argv) {
+    const tempenv::context_parser context(argc, argv);
+}
+
