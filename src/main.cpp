97ef7aa411b83694ee6c9b45694760938c15d259
FuexFollets <fuex9follets@gmail.com>
2023-01-10 19:26:57 -0500

    Added header includes to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,9 @@
-int main() {}
+#include <iostream>
+
+#include "./headers/context_parser.hpp"
+#include "./headers/configuration_file_parser.hpp"
+
+int main() {
+    tempenv::environment_context env_context {};
+}
 
