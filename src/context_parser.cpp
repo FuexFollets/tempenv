489f4b642c46872986209135803d21128ea63f7c
FuexFollets <fuex9follets@gmail.com>
2023-01-10 19:26:37 -0500

    Added encapsulation and removed nested unnamed structs

--- src/context_parser.cpp
+++ src/context_parser.cpp
@@ -16,19 +16,18 @@ namespace tempenv {
         const char* const configuration_directory {"XDG_CONFIG_HOME"};
     };
 
-
     environment_context::environment_context() {
         const std::filesystem::path current_path {std::filesystem::current_path()};
 
-        environment_variables.last_test_path = std::getenv(env_variable_names::last_test_path);
-        environment_variables.last_working_directory = std::getenv(env_variable_names::last_working_directory);
+        _last_test_path = std::getenv(env_variable_names::last_test_path);
+        _last_working_directory = std::getenv(env_variable_names::last_working_directory);
 
         std::filesystem::path tempenv_dotfile_path {current_path};
         tempenv_dotfile_path /= ".tempenv";
 
-        environment_files.is_in_testing_directory = std::filesystem::exists(tempenv_dotfile_path);
+        _is_in_testing_directory = std::filesystem::exists(tempenv_dotfile_path);
 
-        if (!environment_files.is_in_testing_directory) { // Set `environment_variables.last_working_directory`
+        if (!_is_in_testing_directory) { // Set `environment_variables.last_working_directory`
             std::string environment_variable_setting_string {};
 
             environment_variable_setting_string +=
@@ -43,13 +42,13 @@ namespace tempenv {
         configuration_path /= "tempenv";
         configuration_path /= ".tempenv.conf";
 
-        environment_files.config_file_exists = std::filesystem::exists(configuration_path);
+        _config_file_exists = std::filesystem::exists(configuration_path);
 
-        if (environment_files.config_file_exists) { // Read configuration file `$XDG_CONFIG_HOME/tempenv/tempenv.conf`
-            environment_files.config_directory_path = configuration_path;
+        if (_config_file_exists) { // Read configuration file `$XDG_CONFIG_HOME/tempenv/tempenv.conf`
+            _config_directory_path = configuration_path;
             std::ifstream file_contents_stream {configuration_path, std::ios::in};
 
-            file_contents_stream >> environment_files.config_file_contents;
+            file_contents_stream >> _config_file_contents;
         }
     }
 }
--- src/headers/context_parser.hpp
+++ src/headers/context_parser.hpp
@@ -5,22 +5,29 @@
 #include <string>
 
 namespace tempenv {
-    struct environment_context {
-        struct {
-            std::filesystem::path last_test_path {}; // Directory to the last time in a test. Reads from `$__TEMPENV_LAST_TEST_PATH`
-            std::filesystem::path last_working_directory {}; // String path to the last working directory. Reads from `$__TEMPENV_BEFORE_TEST_PATH`
-        } environment_variables;
+    class environment_context {
+        // Directory to the last time in a test. Reads from `$__TEMPENV_LAST_TEST_PATH`
+        std::filesystem::path _last_test_path {};
+        // String path to the last working directory. Reads from `$__TEMPENV_BEFORE_TEST_PATH`
+        std::filesystem::path _last_working_directory {};
+        bool _is_in_testing_directory {}; // Determines if the current directory has a `.tempenv` file
+        // Directory of the configuration file.
+        std::filesystem::path _config_directory_path {};
+        std::string _config_file_contents {};
+        bool _config_file_exists {};
+        // Directory where the testing directory will be created.
+        std::filesystem::path _temp_directory_path {};
 
-        struct {
-            bool is_in_testing_directory {}; // Determines if the current directory has a `.tempenv` file
-
-            // Read from `~/.config/tempenv`
-            std::filesystem::path config_directory_path {}; // Directory of the configuration file.
-            std::string config_file_contents {}; // Contents of the configuration file
-            bool config_file_exists {};
-            std::filesystem::path temp_directory_path {}; // Directory where the testing directory will be created.
-        } environment_files;
+        public:
 
         environment_context();
+
+        [[nodiscard]] std::filesystem::path last_test_path() const;
+        [[nodiscard]] std::filesystem::path last_working_directory() const;
+        [[nodiscard]] std::filesystem::path config_directory_path() const;
+        [[nodiscard]] std::filesystem::path temp_directory_path() const;
+        [[nodiscard]] std::string config_file_contents() const;
+        [[nodiscard]] bool config_file_exists() const;
+        [[nodiscard]] bool is_in_testing_directory() const;
     };
 } // End of namespace tempenv
