a919eafc09aaf80334cb8ddf8df7c51f279d788b
FuexFollets <fuex9follets@gmail.com>
2023-01-12 10:25:47 -0500

    Switched to nlohmann::json for configuration file contents

--- src/headers/context_parser.hpp
+++ src/headers/context_parser.hpp
@@ -2,8 +2,11 @@
 
 #include <filesystem>
 #include <string>
+#include <nlohmann/json.hpp>
 
 namespace tempenv {
+    std::filesystem::path home_directory_path();
+
     class environment_context {
         // Directory to the last time in a test. Reads from `$__TEMPENV_LAST_TEST_PATH`
         std::filesystem::path _last_test_path {};
@@ -12,7 +15,7 @@ namespace tempenv {
         bool _is_in_testing_directory {}; // Determines if the current directory has a `.tempenv` file
         // Directory of the configuration file.
         std::filesystem::path _config_directory_path {};
-        std::string _config_file_contents {};
+        nlohmann::json _config_file_contents {};
         bool _config_file_exists {};
         // Directory where the testing directory will be created.
         std::filesystem::path _temp_directory_path {};
@@ -28,6 +31,6 @@ namespace tempenv {
         [[nodiscard]] std::filesystem::path temp_directory_path() const;
         [[nodiscard]] std::string config_file_contents() const;
         [[nodiscard]] bool config_file_exists() const;
-        [[nodiscard]] bool is_in_testing_directory() const;
+        [[nodiscard]] bool is_in_testing_directory() const; // Works
     };
 } // End of namespace tempenv
