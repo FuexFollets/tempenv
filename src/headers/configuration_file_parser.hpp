f24ede406a8dfe9f3792cf27e145f7fc26f603e0
FuexFollets <fuex9follets@gmail.com>
2023-01-12 10:52:55 -0500

    Added changes to contructor for encapsulation name changes

--- src/headers/configuration_file_parser.hpp
+++ src/headers/configuration_file_parser.hpp
@@ -11,8 +11,8 @@ namespace tempenv {
         std::filesystem::path _tests_location;
         bool _is_valid_tests_location_provided {};
 
-        std::vector<std::string> _copy_with;
-        std::vector<std::string> _execute_in_test_directory;
+        std::vector<std::filesystem::path> _copy_with;
+        std::vector<std::vector<std::string>> _execute_in_test_directory;
 
         std::vector<std::string> _invalid_presets;
 
@@ -21,6 +21,12 @@ namespace tempenv {
         configuration_file() = default;
 
         configuration_file(const nlohmann::json& configuration_json_file, const std::vector<std::string>& chosen_preset_names);
+
+        [[nodiscard]] bool is_valid_tests_location_provided() const;
+        [[nodiscard]] std::filesystem::path tests_location() const;
+        [[nodiscard]] std::vector<std::string> copy_with() const;
+        [[nodiscard]] std::vector<std::vector<std::string>> execute_in_test_directory() const;
+        [[nodiscard]] std::vector<std::string> invalid_presets() const;
     };
 }
 
