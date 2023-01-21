7dd6e4c05f67762bcd17610637ed1e29dd01ef77
FuexFollets <fuex9follets@gmail.com>
2023-01-21 12:49:45 -0500

    Added better formatting

--- src/configuration_file_parser.cpp
+++ src/configuration_file_parser.cpp
@@ -7,7 +7,8 @@
 #include "./headers/configuration_file_parser.hpp"
 
 namespace tempenv {
-    configuration_file::configuration_file(const nlohmann::json& configuration_json_file, const std::vector<std::string>& chosen_preset_names) :
+    configuration_file::configuration_file(const nlohmann::json& configuration_json_file,
+            const std::vector<std::string>& chosen_preset_names) :
         _is_valid_tests_location_provided {configuration_json_file.contains("tests-location")} {
 
         if (_is_valid_tests_location_provided) {
@@ -16,13 +17,15 @@ namespace tempenv {
                 .get<std::filesystem::path>();
         }
 
-        if (!configuration_json_file.contains("presets")) { return; } // Configuration file does not contain presets section
+        // Configuration file does not contain presets section
+        if (!configuration_json_file.contains("presets")) { return; }
 
         const auto& configuration_presets {configuration_json_file.at("presets")};
         const auto configuration_preset_items {configuration_presets.items()};
         std::vector<std::string> valid_presets;
 
-        for (const std::string& chosen_preset: chosen_preset_names) { // Separate valid and invalid chosen presets
+        // Separate valid and invalid chosen presets
+        for (const std::string& chosen_preset: chosen_preset_names) { 
             if (std::any_of(configuration_preset_items.begin(), configuration_preset_items.end(),
                     [&] (const auto& preset_item) { return preset_item.key() == chosen_preset; })) {
                 valid_presets.push_back(chosen_preset);
@@ -32,11 +35,17 @@ namespace tempenv {
         for (const std::string& valid_preset: valid_presets) {
             const auto configured_preset {configuration_presets.at(valid_preset)};
 
-            const auto paths_to_copy_with {configured_preset.at("copy-with").get<std::vector<std::filesystem::path>>()};
-            const auto executed {configured_preset.at("execute-in-test-directory").get<std::vector<std::vector<std::string>>>()};
+            const auto paths_to_copy_with {configured_preset.at("copy-with")
+                .get<std::vector<std::filesystem::path>>()};
 
-            _copy_with.insert(_copy_with.end(), paths_to_copy_with.begin(), paths_to_copy_with.end());
-            _execute_in_test_directory.insert(_execute_in_test_directory.begin(), executed.begin(), executed.end());
+            const auto executed {configured_preset.at("execute-in-test-directory")
+                .get<std::vector<std::vector<std::string>>>()};
+
+            _copy_with.insert(_copy_with.end(), paths_to_copy_with.begin(),
+                    paths_to_copy_with.end());
+
+            _execute_in_test_directory.insert(_execute_in_test_directory.begin(), executed.begin(),
+                    executed.end());
         }
     }
 
