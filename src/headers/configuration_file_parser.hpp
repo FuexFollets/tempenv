7df724b9a01fb7d077e30393234dfb5c213c95e9
FuexFollets <fuex9follets@gmail.com>
2023-01-22 11:29:27 -0500

    Rewrote headers for configuration file parser

--- src/headers/configuration_file_parser.hpp
+++ src/headers/configuration_file_parser.hpp
@@ -4,27 +4,39 @@
 #include <vector>
 #include <string>
 #include <exception>
-#include <yaml.h>
+#include <toml.hpp>
 
 namespace tempenv {
     class configuration_file {
-        std::filesystem::path _tests_location;
-        bool _is_valid_tests_location_provided {};
-
-        std::vector<std::filesystem::path> _copy_with;
-        std::vector<std::vector<std::string>> _execute_in_test_directory;
-
-        std::vector<std::string> _invalid_presets;
 
         public:
 
+        struct preset {
+            private:
+
+            std::vector<std::filesystem::path> _copy_with {};
+            std::vector<std::vector<std::string>> _execute_in_test_directory {};
+
+            public:
+
+            std::vector<std::filesystem::path> copy_with();
+            std::vector<std::vector<std::string>> execute_in_test_directory();
+        };
+
         configuration_file() = default;
+        explicit configuration_file(const toml::value& toml_contents);
 
         [[nodiscard]] bool is_valid_tests_location_provided() const;
         [[nodiscard]] std::filesystem::path tests_location() const;
-        [[nodiscard]] std::vector<std::filesystem::path> copy_with() const;
-        [[nodiscard]] std::vector<std::vector<std::string>> execute_in_test_directory() const;
-        [[nodiscard]] std::vector<std::string> invalid_presets() const;
+        [[nodiscard]] std::vector<preset> available_presets() const;
+
+        private:
+
+        std::filesystem::path _tests_location {};
+        bool _is_valid_tests_location_provided {};
+
+        std::vector<preset> _available_presets {};
+
     };
 }
 
