1a6f2807cee31b73638e8e99590440f92919af06
FuexFollets <fuex9follets@gmail.com>
2023-01-24 17:57:06 -0500

    Remvoed code from configuration_file_parser.cpp

--- src/configuration_file_parser.cpp
+++ src/configuration_file_parser.cpp
@@ -1,10 +1,7 @@
 #include <string>
-#include <toml.hpp>
+#include <toml++/toml.h>
 
 #include "./headers/configuration_file_parser.hpp"
 
 namespace tempenv {
-    configuration_file::configuration_file(const toml::value& parsed_toml) {
-        const auto& presets {toml::find(parsed_toml, "presets")};
-    }
 }
