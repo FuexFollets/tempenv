15cfaf0b65f19a7ea3dbf790d11478a40b6ed36a
FuexFollets <fuex9follets@gmail.com>
2023-01-31 12:39:32 -0500

    Rewrote vartest for toml

--- tests/test_vartest.cpp
+++ tests/test_vartest.cpp
@@ -1,57 +1,19 @@
-#include <fstream>
 #include <iostream>
-#include <string>
-#include <iterator>
-#include <nlohmann/json.hpp>
+#include <toml++/toml.h>
 
 #include "../src/headers/context_parser.hpp"
 #include "../src/headers/configuration_file_parser.hpp"
 
 int main() {
     std::cout << std::boolalpha;
-    tempenv::environment_context env_context {};
-    nlohmann::json example_configuration_json;
-    std::ifstream {"./../example/example-configuration.json"} >> example_configuration_json;
 
-    tempenv::configuration_file configuration_file {
-        example_configuration_json,
-        std::vector<std::string>{"hello", "an-invalid-preset"}
-    };
-
-    std::cout << "Environent conetext tests: \n\n"
-        << "Home directory: " << tempenv::home_directory_path() << '\n'
-        << "Are we in a testing directory? " << env_context.is_in_testing_directory() << '\n'
-        << "Configuration directory path: " << env_context.config_directory_path() << '\n'
-        << "Path to most recent test: " << env_context.last_test_path() << '\n'
-        << "Path to working directory before in current test: " << env_context.last_working_directory() << '\n'
-        << "Does the configuration file exist? " << env_context.config_file_exists() << '\n'
-        << "Contents of the config file: \n" << env_context.config_file_contents() << '\n';
-
-    auto copy_with {configuration_file.copy_with()};
-    auto invalid_presets {configuration_file.invalid_presets()};
-
-    std::cout << "\n\nConfiguration file tests: \n\n";
-    std::cout << "Files to copy with: ";
-    std::copy(
-            copy_with.begin(), copy_with.end(),
-            std::ostream_iterator<std::string>(std::cout, ", "));
-
+    tempenv::configuration_file example_config {toml::parse_file("./example.toml")};
+    std::vector<std::string> chosen_presets {};
+    const auto[copy_with, execute_in_test] {example_config.files_to_copy_and_execute(chosen_presets)};
 
     std::cout
-        << "\nIs a valid tests location specified? " << configuration_file.is_valid_tests_location_provided()
-        << "\nTests location: " << configuration_file.tests_location()
-        << "\nInvalid presets: ";
-
-    std::copy(
-            invalid_presets.begin(), invalid_presets.end(),
-            std::ostream_iterator<std::string>(std::cout, ", "));
-
-    std::cout << "\nExecuted in directory: ";
-    for (auto&& command: configuration_file.execute_in_test_directory()) {
-        std::copy(
-                command.begin(), command.end(),
-                std::ostream_iterator<std::string>(std::cout, ", "));
-    }
+        << "Is a valid test location provided? " << example_config.is_valid_tests_location_provided() << '\n'
+        << "Valid tests location: " << example_config.tests_location() << '\n';
 
     std::cout << '\n';
 }
