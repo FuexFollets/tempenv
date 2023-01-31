24afb321a2665de50e1e4003c13b3341b7ebf4b4
FuexFollets <fuex9follets@gmail.com>
2023-01-31 12:38:08 -0500

    Added test for the new tomlplusplus library

--- tests/test_tomlplusplus.cpp
+++ tests/test_tomlplusplus.cpp
@@ -4,7 +4,17 @@
 #include <toml++/toml.h>
 
 int main() {
-    const auto example {toml::parse_file("./example.toml")};
+    const auto example {
+        toml::parse_file("/home/fuexfollets/Projects/tempenv/tests/example.toml")};
 
-    std::cout << "Tests location: " << example["tests_location"];
+    /*
+    std::cout << "Parsed toml file: \n"
+        << example << "\n\n";
+    */
+
+    std::cout
+        << "Tests location: " << example["tests_location"] << '\n'
+        << "Presets forall: "
+        << example["forall_presets"]["execute_in_test_directory"][0][0] << '\n';
+
 }
