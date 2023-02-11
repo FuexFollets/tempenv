39ebe296389cb4b17b54d942927d2709dd9f7189
FuexFollets <fuex9follets@gmail.com>
2023-02-11 15:14:21 -0500

    Added test for testing file parse

--- tests/test_vartest.cpp
+++ tests/test_vartest.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
 #include <toml++/toml.h>
 
-#include "../src/headers/context_parser.hpp"
 #include "../src/headers/configuration_file_parser.hpp"
 
 int main() {
     std::cout << std::boolalpha;
 
-    tempenv::configuration_file example_config {toml::parse_file("./example.toml")};
-    std::vector<std::string> chosen_presets {};
-    const auto[copy_with, execute_in_test] {example_config.files_to_copy_and_execute(chosen_presets)};
+    tempenv::configuration_file example_config {toml::parse_file("/home/fuexfollets/Projects/tempenv/example/example_configuration.toml")};
 
     std::cout
-        << "Is a valid test location provided? " << example_config.is_valid_tests_location_provided() << '\n'
-        << "Valid tests location: " << example_config.tests_location() << '\n';
+        << "Is a valid tests location provided? " << example_config.is_valid_tests_location_provided() << '\n'
+        << "Tests loaction: " << example_config.tests_location() << '\n';
 
     std::cout << '\n';
 }
