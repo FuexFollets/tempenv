746961e9c0fa7770e8db064bc8dc65da6e97286d
FuexFollets <fuex9follets@gmail.com>
2023-02-11 15:16:15 -0500

    Fixed segfault and file parsing

--- src/headers/configuration_file_parser.hpp
+++ src/headers/configuration_file_parser.hpp
@@ -1,50 +1,22 @@
 #pragma once
 
 #include <filesystem>
-#include <vector>
-#include <utility>
 #include <toml++/toml.h>
 
 namespace tempenv {
     class configuration_file {
-
         public:
 
-        struct preset {
-            std::string _name {};
-            std::vector<std::filesystem::path> _copy_with {};
-            std::vector<std::vector<std::string>> _execute_in_test_directory {};
-
-            [[nodiscard]] std::string name() const;
-            [[nodiscard]] std::vector<std::filesystem::path> copy_with() const;
-            [[nodiscard]] std::vector<std::vector<std::string>> execute_in_test_directory() const;
-
-            preset() = default;
-            preset(std::string&& preset_name, const toml::table& preset_table);
-        };
+        [[nodiscard]] std::filesystem::path tests_location() const;
+        [[nodiscard]] bool is_valid_tests_location_provided() const;
 
         configuration_file() = default;
         explicit configuration_file(const toml::table& parsed_config_file);
-
-        [[nodiscard]] bool is_valid_tests_location_provided() const;
-        [[nodiscard]] std::filesystem::path tests_location() const;
-        [[nodiscard]] std::vector<preset> available_presets() const;
-
-        struct copy_and_execute {
-            std::vector<std::filesystem::path> copy_with {};
-            std::vector<std::vector<std::string>> execute_in_test_directory {};
-        };
-
-        [[nodiscard]] copy_and_execute
-            files_to_copy_and_execute(const std::vector<std::string>& chosen_presets) const;
 
         private:
 
         std::filesystem::path _tests_location {};
         bool _is_valid_tests_location_provided {};
-
-        std::vector<preset> _available_presets {};
-        preset _forall_presets {};
     };
 }
 
