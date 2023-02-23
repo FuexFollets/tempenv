70fd18aa0fe925981a68ff0e7cbe21aa2cdef2ac
FuexFollets <fuex9follets@gmail.com>
2023-02-23 12:10:24 -0500

    Added header file for decision maker

---
+++ src/headers/decision_maker.hpp
@@ -0,0 +1,25 @@
+#include <filesystem>
+#include <string>
+
+#include <argparse/argparse.hpp>
+#include <toml++/toml.h>
+
+#include "configuration_file_parser.hpp"
+#include "argparse_program.hpp"
+#include "defaults.hpp"
+
+namespace tempenv {
+    class decision_maker {
+        private:
+
+        std::filesystem::path _tests_location {};
+        std::string _test_name {};
+
+        public:
+
+        [[nodiscard]] std::filesystem::path tests_location() const;
+        [[nodiscard]] std::string test_name() const;
+
+        decision_maker(configuration_file parsed_config_file, tempenv_argument_parser parsed_arguments);
+    };
+}
