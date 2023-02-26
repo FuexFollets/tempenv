a5a3e177e4a336a4ab331e1426c4f2e202f4f651
FuexFollets <fuex9follets@gmail.com>
2023-02-26 12:19:48 -0500

    Removed .cache files

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
+#include <cstdlib>
 #include <iostream>
 #include <functional>
 #include <string>
 
-#include "./headers/context_parser.hpp"
-#include "./headers/configuration_file_parser.hpp"
+#include "headers/actions.hpp"
 
-int main() {
+int main(int argc, const char** argv) {
+    tempenv::tempenv_argument_parser parsed_arguments {argc, argv};
+    tempenv::decision_maker chosen_options {parsed_arguments};
+    tempenv::make_decisions(chosen_options);
+
+    return EXIT_SUCCESS;
 }
-
-
