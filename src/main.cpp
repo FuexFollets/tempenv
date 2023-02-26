46f62b0cacb255c749b8c21dd4d7cf50447940e6
FuexFollets <fuex9follets@gmail.com>
2023-02-26 12:20:31 -0500

    Wrote main c++ file for tempenv

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,9 @@
 #include "headers/actions.hpp"
 
 int main(int argc, const char** argv) {
-    tempenv::tempenv_argument_parser parsed_arguments {argc, argv};
-    tempenv::decision_maker chosen_options {parsed_arguments};
+    const tempenv::tempenv_argument_parser parsed_arguments {argc, argv};
+    const tempenv::decision_maker chosen_options {parsed_arguments};
+
     tempenv::make_decisions(chosen_options);
 
     return EXIT_SUCCESS;
