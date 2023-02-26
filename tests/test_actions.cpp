6232ed50e9290fe12aaacdd95c0b2e23a62da4c9
FuexFollets <fuex9follets@gmail.com>
2023-02-26 09:39:43 -0500

    Fixed 'test_actions' test for one-argument decision maker constuctor

--- tests/test_actions.cpp
+++ tests/test_actions.cpp
@@ -4,9 +4,7 @@
 
 int main(int argc, const char** argv) {
     tempenv::tempenv_argument_parser parsed_arguments {argc, argv};
-    tempenv::configuration_file configuration_file {toml::parse_file("example/example_configuration.toml")};
-
-    tempenv::decision_maker chosen_options {configuration_file, parsed_arguments};
+    tempenv::decision_maker chosen_options {parsed_arguments};
 
     tempenv::make_decisions(chosen_options);
 }
