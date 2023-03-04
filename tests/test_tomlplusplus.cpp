2a9f61d49f63c3195fd436caf9bb2b5304489c95
FuexFollets <fuex9follets@gmail.com>
2023-03-04 12:47:52 -0500

    Made additional feature tests for toml++

--- tests/test_tomlplusplus.cpp
+++ tests/test_tomlplusplus.cpp
@@ -12,4 +12,16 @@ int main() {
     for (const auto& path: *example["forall_presets"]["copy_with"].as_array()) {
         std::cout << path.value_or<std::string>({});
     }
+
+    std::cout << '\n';
+
+    const auto& all_presets {example["presets"]};
+
+    std::cout << "All presets:\n" << all_presets << '\n';
+
+    for (auto&&[preset_name, value]: *all_presets.as_table()) {
+        std::cout
+            << "Preset name: " << preset_name << '\n'
+            << "Preset value:\n" << *value.as_table() << "\n\n\n";
+    }
 }
