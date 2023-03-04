4d82e21a03ae0f73b09f8545fc8720d6e0f9d9ec
FuexFollets <fuex9follets@gmail.com>
2023-03-04 13:05:53 -0500

    Added prototyped functions for presets

--- src/headers/argparse_program.hpp
+++ src/headers/argparse_program.hpp
@@ -16,6 +16,7 @@ namespace tempenv {
         std::string _test_name {};
         std::optional<std::filesystem::path> _test_directory {};
         std::optional<std::filesystem::path> _configuration_file_location {};
+        std::vector<std::string> _selected_preset_names {};
 
         argparse::ArgumentParser program;
 
@@ -26,6 +27,7 @@ namespace tempenv {
         [[nodiscard]] std::string test_name() const;
         [[nodiscard]] std::optional<std::filesystem::path> test_directory() const;
         [[nodiscard]] std::optional<std::filesystem::path> configuration_file_location() const;
+        [[nodiscard]] std::vector<std::string> selected_preset_names() const;
 
         tempenv_argument_parser() = default;
         tempenv_argument_parser(int argc, const char** argv);
