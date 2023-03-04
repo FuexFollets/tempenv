75ff39acf749af3c030a7e47c8277393cae604ff
FuexFollets <fuex9follets@gmail.com>
2023-03-04 11:05:43 -0500

    Added tests for configuration file parsing

---
+++ tests/test_configuration_file_parse.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+#include <toml++/toml.h>
+
+#include "../src/headers/configuration_file_parser.hpp"
+
+template <typename T> void print_vector(const std::vector<T>& printed_vector) {
+    for (const T& value: printed_vector) {
+        std::cout << value << " ";
+    }
+}
+
+
+template <typename T> void print_vector(const std::vector<std::vector<T>>& printed_vector) {
+    for (const std::vector<T>& value: printed_vector) {
+        print_vector<T>(value);
+        std::cout << '\n';
+    }
+}
+
+void print_preset(const tempenv::preset& printed_preset) {
+    std::cout << "Preset name: " << printed_preset.name() << '\n'
+        << "Preset copy with:\n";
+
+    print_vector(printed_preset.copy_with());
+
+    std::cout << "\nExecute in test directory:\n";
+
+    print_vector(printed_preset.execute_in_test_directory());
+}
+
+int main(int argc, const char** argv) {
+    if (argc < 2) {
+        std::cerr << "Provide path argument.\nExiting.\n";
+        exit(128);
+    }
+
+    const char* file_path_string {*std::next(argv)};
+
+    tempenv::configuration_file parsed_config_file {toml::parse_file(file_path_string)};
+
+    std::cout << "Parsing file at path: " << file_path_string << '\n';
+
+    std::cout
+        << "Is a valid tests location provided? " << parsed_config_file.is_valid_tests_location_provided() << '\n'
+        << "If so, test location: " << parsed_config_file.tests_location() << '\n';
+
+    std::cout << "Forall presets\n";
+    print_preset(parsed_config_file.forall_presets());
+
+    std::cout << "\nAll presets\n";
+    for (const tempenv::preset& parsed_preset: parsed_config_file.all_presets()) {
+        print_preset(parsed_preset);
+
+        std::cout << '\n';
+    }
+}
--- tests/test_tomlplusplus.cpp
+++ tests/test_tomlplusplus.cpp
@@ -1,20 +1,15 @@
-#include <fstream>
 #include <iostream>
-#include <string>
+
 #include <toml++/toml.h>
+
+#include "../src/headers/configuration_file_parser.hpp"
 
 int main() {
     const auto example {
-        toml::parse_file("/home/fuexfollets/Projects/tempenv/example/example_configuration.toml")};
+        toml::parse_file("/home/fuexfollets/Projects/tempenv/example/example_configuration.toml")
+    };
 
-    /*
-    std::cout << "Parsed toml file: \n"
-        << example << "\n\n";
-    */
-
-    std::cout
-        << "Tests location: " << example["tests_location"] << '\n'
-        << "Presets forall: "
-        << example["forall_presets"]["execute_in_test_directory"][0][0] << '\n';
-
+    for (const auto& path: *example["forall_presets"]["copy_with"].as_array()) {
+        std::cout << path.value_or<std::string>({});
+    }
 }
