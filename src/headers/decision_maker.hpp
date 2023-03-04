36ca196cbe644d0dc326f6a29ad35857e4d9fe85
FuexFollets <fuex9follets@gmail.com>
2023-03-04 13:39:24 -0500

    Added decision maker member variables for presets

--- src/headers/decision_maker.hpp
+++ src/headers/decision_maker.hpp
@@ -19,12 +19,16 @@ namespace tempenv {
         bool _default_test_location_chosen {};
         std::filesystem::path _tests_location {};
         std::string _test_name {};
+        std::vector<std::filesystem::path> _copy_with {};
+        std::vector<std::vector<std::string>> _execute_in_test_directory {};
 
         public:
 
         [[nodiscard]] bool default_test_location_chosen() const;
         [[nodiscard]] std::filesystem::path tests_location() const;
         [[nodiscard]] std::string test_name() const;
+        [[nodiscard]] std::vector<std::filesystem::path> copy_with() const;
+        [[nodiscard]] std::vector<std::vector<std::string>> execute_in_test_directory() const;
 
         explicit decision_maker(const tempenv_argument_parser& parsed_arguments);
     };
