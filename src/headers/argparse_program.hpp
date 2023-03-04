a2402461f8f30a13e95cdb691406b225929d08c4
FuexFollets <fuex9follets@gmail.com>
2023-03-04 13:42:03 -0500

    Added optional wrapper to argparse preset names

--- src/headers/argparse_program.hpp
+++ src/headers/argparse_program.hpp
@@ -16,7 +16,7 @@ namespace tempenv {
         std::string _test_name {};
         std::optional<std::filesystem::path> _test_directory {};
         std::optional<std::filesystem::path> _configuration_file_location {};
-        std::vector<std::string> _selected_preset_names {};
+        std::optional<std::vector<std::string>> _selected_preset_names {};
 
         argparse::ArgumentParser program;
 
@@ -27,7 +27,7 @@ namespace tempenv {
         [[nodiscard]] std::string test_name() const;
         [[nodiscard]] std::optional<std::filesystem::path> test_directory() const;
         [[nodiscard]] std::optional<std::filesystem::path> configuration_file_location() const;
-        [[nodiscard]] std::vector<std::string> selected_preset_names() const;
+        [[nodiscard]] std::optional<std::vector<std::string>> selected_preset_names() const;
 
         tempenv_argument_parser() = default;
         tempenv_argument_parser(int argc, const char** argv);
